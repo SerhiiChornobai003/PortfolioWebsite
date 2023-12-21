2595cbe0bf5847ff731249dc92577fa23d3b11b0
Serhii <sergkraken2003@gmail.com>
2023-12-21 11:26:26 +0200

    Project finished

--- dist/js/script.js
+++ dist/js/script.js
@@ -24,3 +24,36 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+$('.feedback__form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() { 
+        $(this).find("input").val("");
+        
+        $('form').trigger('reset');
+        
+    });
+    return false;
+});
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close'),
+      menuLinks = document.querySelectorAll('.menu__link');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        // remove the 'active' class from the menu
+        menu.classList.remove('active');
+    });
+});
